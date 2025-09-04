74f2516cac8249427961cbf599966105f33ca2c0
Shekhar Bisla <shekharbisla011@gmail.com>
2025-09-04 15:10:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from app import create_app
+import os
+
+# Create Flask app using factory
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # PORT: Cloud hosting (Render/Railway/Replit) will inject this
+    port = int(os.environ.get("PORT", 5000))
+    # ENV: If not set, defaults to "dev" (see config.py)
+    env = os.environ.get("ENV", "dev")
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=(env == "dev")   # debug true only in dev
+    )
